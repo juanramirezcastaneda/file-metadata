a443d2a17c46b72bfa5cf442ab77cadb0189b103
juanramirezcastaneda <juanramirezcastaneda@gmail.com>
2020-02-20 17:27:39 +0100

    add server action to retrieve index

---
+++ server.js
@@ -0,0 +1,54 @@
+'use-strict';
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const cors = require('cors');
+var port = process.env.PORT || 8000;
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/exercise-track', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const Schema = mongoose.Schema;
+
+app.use(express.static(__dirname));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+});
+
+// Not found middleware
+app.use((req, res, next) => {
+    return next({ status: 404, message: 'not found' })
+});
+
+// Error Handling middleware
+app.use((err, req, res, next) => {
+    let errCode, errMessage;
+
+    if (err.errors) {
+        // mongoose validation error
+        errCode = 400 // bad request
+        const keys = Object.keys(err.errors);
+        // report the first validation error
+        errMessage = err.errors[keys[0]].message;
+    } else {
+        // generic or custom error
+        errCode = err.status || 500;
+        errMessage = err.message || 'Internal Server Error';
+    }
+    res.status(errCode).type('txt').send(errMessage);
+});
+
+app.listen(port, function () {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
